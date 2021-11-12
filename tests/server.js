77eb6589a5f049b1edeefabffe8b68a7341da654
Carlos Luis Castro Márquez <clcastro87@users.noreply.github.com>
2021-11-11 22:53:19 -0500

    Removing old code.

--- tests/server.js
+++ tests/server.js
@@ -1,16 +1,22 @@
 /**
- * Copyright 2015
- * Created by Carlos on 12/29/2015.
+ * @author: Carlos Luis Castro Márquez
  */
+"use strict";
 
-var express = require('../node_modules/express');
-var logger = require('morgan');
-var i18n = require('../src/middleware');
-var app = express();
-app.use(logger('dev'));
-app.use(i18n.localize({endpointEnabled: true, path: __dirname + '/i18n', queryStringEnabled: true}));
-app.use('/locals', function(req, res) {
-	res.json(res.locals.i18n);
+const express = require("../node_modules/express");
+const logger = require("morgan");
+const i18n = require("../src/middleware");
+const app = express();
+app.use(logger("dev"));
+app.use(
+    i18n.localize({
+        endpointEnabled: true,
+        path: __dirname + "/i18n",
+        queryStringEnabled: true,
+    })
+);
+app.use("/locals", function (req, res) {
+    res.json(res.locals.i18n);
 });
 app.listen(4000);
 
